3c9c413691431ac7264b76c917012f684355d4ab
LuG3Zz <1403338195@qq.com>
2020-07-10 11:16:26 +0800

    练习一code

---
+++ 练习一/练习1.9.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main{
+	int sum = 0,val = 50;
+while (val<=100) {
+	sum += val;
+	val += 1;
+
+}
+std::cout << "sum of 50 to 100 inclusive is " << sum << std::endl;
+	return 0;
+}
